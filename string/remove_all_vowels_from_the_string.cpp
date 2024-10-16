9e116a258e5853cceb096e538e13dca1f5fbf75c
Nishayadav02 <nishayadav5752@gmail.com>
2024-10-16 08:12:11 +0530

    code

--- string/remove_all_vowels_from_the_string.cpp
+++ string/remove_all_vowels_from_the_string.cpp
@@ -2,16 +2,44 @@
 #include <string>
 using namespace std;
 
-string removeVowels(const string& str) {
-    string result = "";
+// string removeVowels(const string& str) {
+//     string result = "";
+//     for (int i = 0; i < str.length(); i++) {
+//         // Check if the character is not a vowel
+//         if (str[i] != 'a' && str[i] != 'e' && str[i] != 'i' && str[i] != 'o' && str[i] != 'u' &&
+//             str[i] != 'A' && str[i] != 'E' && str[i] != 'I' && str[i] != 'O' && str[i] != 'U') {
+//             result += str[i]; // Add non-vowel characters to the result
+//         }
+//     }
+//     return result;
+// }
+
+// int main() {
+//     string input;
+//     cout << "Enter a string: ";
+//     getline(cin, input);
+
+//     string noVowels = removeVowels(input);
+//     cout << "String without vowels: " << noVowels << endl;
+
+//     return 0;
+// }
+
+
+
+void removeVowels(string &str) {
+    int index = 0; // To track the position of non-vowel characters
+
     for (int i = 0; i < str.length(); i++) {
         // Check if the character is not a vowel
         if (str[i] != 'a' && str[i] != 'e' && str[i] != 'i' && str[i] != 'o' && str[i] != 'u' &&
             str[i] != 'A' && str[i] != 'E' && str[i] != 'I' && str[i] != 'O' && str[i] != 'U') {
-            result += str[i]; // Add non-vowel characters to the result
+            str[index++] = str[i]; // Overwrite the string with non-vowel characters
         }
     }
-    return result;
+
+    // Resize the string to the length of non-vowel characters
+    str.resize(index);
 }
 
 int main() {
@@ -19,8 +47,8 @@ int main() {
     cout << "Enter a string: ";
     getline(cin, input);
 
-    string noVowels = removeVowels(input);
-    cout << "String without vowels: " << noVowels << endl;
+    removeVowels(input);
+    cout << "String without vowels: " << input << endl;
 
     return 0;
 }
