34e75569e8cd0737a97b677eb27ca36a77995edf
Nishayadav02 <nishayadav5752@gmail.com>
2024-10-28 11:17:41 +0530

    code

--- linked_list/Linked_LIst.cpp
+++ linked_list/Linked_LIst.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 class Node{
@@ -19,9 +20,26 @@ class Node{
     A1.next = NULL;
 }*/
 
-int main(){
+/*int main(){
     //dynamic node creation
     Node * Head = new Node(4);
     // Head -> data = 4;
     // Head -> next = NULL;
+}*/
+
+Node* convertArr2LL(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for(int i = 1 ; i < arr.size(); i++){
+        Node* temp = new Node(arr[i]);
+        mover -> next = temp;
+        mover = temp;
+    }
+    return head;
+}
+
+int main(){
+    vector<int> arr = {2 , 5 , 7 , 9};
+    Node* head = convertArr2LL(arr);
+    cout << head -> data;
 }
