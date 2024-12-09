f6ba65ea5fdf05e63e56ea2962144197820497e0
Nishayadav02 <nishayadav5752@gmail.com>
2024-12-09 23:27:05 +0530

    code

---
+++ recursion/subsequences.cpp
@@ -0,0 +1,16 @@
+// #include<iostream>
+// using namespace std;
+
+// void solve(int index , vector<int>&ds , int arr[] , int n , vector<vector<int>>&result){
+    
+// }
+
+// int main()
+// {
+//     int arr[] = {1 , 2, 3};
+//     int n = 3;
+//     vector<int>ds;
+//     vector<vector<int>> result;
+//     solve(0 , ds , arr , n , result);
+//     return 0;
+// }
